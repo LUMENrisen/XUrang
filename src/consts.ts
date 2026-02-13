00a867e025cfaf948a20e7520f106004b468fa45
LUMENrisen <lyc@lumenatelier.top>
2026-02-14 04:27:01 +0800

    feat: people pages using identities and blog posts

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,13 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const identities = [
+  { person: '路过', roomTitle: '风来' },
+  { person: '路上', roomTitle: '花开' },
+  { person: '路明', roomTitle: '又生' },
+  { person: '路炎晨', roomTitle: '成蹊' },
+  { person: '路宅', roomTitle: '若水' },
+  { person: '路痴', roomTitle: '不尽' },
+  { person: '路标', roomTitle: '道' },
+];
